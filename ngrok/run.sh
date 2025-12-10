ba9a9b44040d277fe3715f3dca46ef5aacc99acc
Ramon Cardena <info@ramoncardena.com>
2025-12-10 17:26:37 +0100

    Added additional info when connection stablished.

--- ngrok/run.sh
+++ ngrok/run.sh
@@ -11,12 +11,32 @@ fi
 
 ngrok config add-authtoken "$AUTH_TOKEN"
 
-# Start ngrok on port 8123 (HA default)
-# We use nohup to keep it running or just exec depending on if we want other things.
-# For a simple addon, exec is fine.
-# ngrok http 8123
-# However, to see logs properly we might want to tweak this. 
-# Using bashio to log nicely.
-
 bashio::log.info "Starting ngrok tunnel..."
-exec ngrok http 8123
+
+# Start ngrok in background
+ngrok http 8123 --log stdout > /dev/null &
+NGROK_PID=$!
+
+# Wait for API to be available
+for i in $(seq 1 30); do
+    if curl -s http://localhost:4040/api/tunnels > /dev/null; then
+        break
+    fi
+    sleep 1
+done
+
+# Get URL
+PUBLIC_URL=$(curl -s http://localhost:4040/api/tunnels | jq -r '.tunnels[0].public_url')
+
+if [ "$PUBLIC_URL" != "null" ] && [ -n "$PUBLIC_URL" ]; then
+    bashio::log.info "---------------------------------------------------"
+    bashio::log.info "   "
+    bashio::log.info "   Ngrok Tunnel started!"
+    bashio::log.info "   Public URL: $PUBLIC_URL"
+    bashio::log.info "   "
+    bashio::log.info "---------------------------------------------------"
+else
+    bashio::log.warning "Could not retrieve public URL from ngrok API."
+fi
+
+wait $NGROK_PID
